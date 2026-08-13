5f4470291b02810be946b3ce146c6d551eb2e921
Mark Langen <stravant@gmail.com>
2026-08-13 02:49:22 -0700

    Correct level skip products

--- src/shared/DeveloperProduct.lua
+++ src/shared/DeveloperProduct.lua
@@ -1,8 +1,8 @@
 --!strict
 local DeveloperProduct = {}
 
-DeveloperProduct.Skip1 = 145456721
-DeveloperProduct.Skip3 = 145457553
-DeveloperProduct.Skip5 = 145458069
+DeveloperProduct.Skip1 = 3707832941
+DeveloperProduct.Skip3 = 3707833001
+DeveloperProduct.Skip5 = 3707833067
 
 return DeveloperProduct
